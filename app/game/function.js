f27fe9ee0fabeb2c8b69ba82d4bf378ad46ab5aa
Jacob Sharp <sidewayseightgmod@gmail.com>
2015-12-16 09:22:53 -0500

    added AI, little buggy

--- app/game/function.js
+++ app/game/function.js
@@ -4,7 +4,7 @@ var swingRate = 100;
 var nextSlash = 0;
 var slash;
 var bullet;
-
+var ranOnce = true;
 function gunFire() {
   if (game.time.now > nextFire && bullets.countDead() > 0 && player.ammo > 0) {
     nextFire = game.time.now + fireRate;
@@ -82,17 +82,23 @@ function processHandler(projectile, enemy) {
 Enemy = function(index, game, player, bullets) {
   var x = game.world.randomX;
   var y = game.world.randomY;
-
+  console.log(map.getTileWorldXY(x, y, 128, 128).index);
+  // console.log(x, y);
+  while(map.getTileWorldXY(x, y, 128, 128).index != 13 && map.getTileWorldXY(x, y, 128, 128).index != 14 && map.getTileWorldXY(x, y, 128, 128).index != 8){
+    x = game.world.randomX;
+    y = game.world.randomY;
+  }
   this.game = game;
   this.health = 3;
   this.player = player;
   this.bullets = enemyBullets;
-  this.fireRate = 750;
+  this.fireRate = 2000;
   this.nextFire = 0;
   this.alive = true;
-
+  this.timer = game.time.create();
   this.enemy = game.add.sprite(x, y, 'enemy');
   this.enemy.animations.add('run', [4, 5, 6, 7, 8, 9], true);
+  this.enemy.animations.add('idle', [0, 1, 2, 3], true);
   this.gun = game.add.sprite(x, y, 'gun');
   this.gun.scale.setTo(0.5, 0.5);
   this.enemy.anchor.set(0.5);
@@ -104,10 +110,6 @@ Enemy = function(index, game, player, bullets) {
   this.enemy.body.immovable = false;
   this.enemy.body.collideWorldBounds = true;
   this.enemy.body.bounce.setTo(1, 1);
-
-  // this.enemy.angle = game.rnd.angle();
-
-  game.physics.arcade.velocityFromRotation(this.enemy.rotation, 100, this.enemy.body.velocity);
 };
 Enemy.prototype.damage = function() {
   if (gunEquipped) {
@@ -126,26 +128,55 @@ Enemy.prototype.damage = function() {
 }
 
 Enemy.prototype.update = function() {
-
-  this.enemy.animations.play('run', 10, true);
+  this.timer.start();
   this.game.physics.arcade.collide(this.enemy, layer);
   this.gun.x = this.enemy.x;
   this.gun.y = this.enemy.y;
-  this.gun.rotation = this.game.physics.arcade.angleBetween(this.enemy, this.player);
-  if (this.enemy.x > this.player.x) {
-    this.enemy.scale.x = -1;
-    this.gun.scale.y = -0.5;
-  } else {
-    this.enemy.scale.x = 1;
-    this.gun.scale.y = 0.5;
-  }
+  this.gun.rotation = 0;
   if (this.game.physics.arcade.distanceBetween(this.enemy, this.player) < 300) {
-    if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0) {
-      this.nextFire = this.game.time.now + this.fireRate;
-      var bullet = this.bullets.getFirstDead();
-      bullet.scale.setTo(0.5, 0.5);
-      bullet.reset(this.gun.x, this.gun.y);
-      bullet.rotation = this.game.physics.arcade.moveToObject(bullet, this.player, 500);
+    this.enemy.animations.play('run', 10, true);
+    this.game.physics.arcade.moveToObject(this.enemy, this.player);
+    if (this.enemy.x > this.player.x) {
+      this.enemy.scale.x = -1;
+      this.gun.scale.y = -0.5;
+    } else {
+      this.enemy.scale.x = 1;
+      this.gun.scale.y = 0.5;
+    }
+    this.gun.rotation = this.game.physics.arcade.angleBetween(this.enemy, this.player);
+    if(this.game.physics.arcade.distanceBetween(this.enemy, this.player) < 200){
+      this.enemy.animations.stop();
+      this.enemy.animations.play('idle', 10, true);
+      if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0) {
+        this.nextFire = this.game.time.now + this.fireRate;
+        var bullet = this.bullets.getFirstDead();
+        bullet.body.setSize(32, 32);
+        bullet.scale.setTo(0.5, 0.5);
+        bullet.reset(this.gun.x, this.gun.y);
+        bullet.rotation = this.game.physics.arcade.moveToObject(bullet, this.player, 500);
+      }
+      this.enemy.body.velocity.x = 0;
+      this.enemy.body.velocity.y = 0;
+    }
+  }
+  else{
+    if(this.timer.seconds > 2 || ranOnce === true){
+      ranOnce = false;
+      this.enemy.body.velocity.x = 50 * (Math.round(Math.random()) * 2 - 1) * (Math.round(Math.random()));
+      this.enemy.body.velocity.y = 50 * (Math.round(Math.random()) * 2 - 1) * (Math.round(Math.random()));
+      this.enemy.animations.play('run', 10, true);
+      if(this.enemy.body.velocity.x === 0 && this.enemy.body.velocity.y === 0){
+        this.enemy.animations.play('idle', 10, true);
+      }
+      if(this.enemy.body.velocity.x < 0){
+        this.enemy.scale.x = -1;
+        this.gun.scale.x = -0.5;
+      }
+      else{
+        this.enemy.scale.x = 1;
+        this.gun.scale.x = 0.5;
+      }
+      this.timer.stop();
     }
   }
 
--- app/game/render.js
+++ app/game/render.js
@@ -1,3 +1,3 @@
 function render(){
-
+  // layer.debug= true;
 }
